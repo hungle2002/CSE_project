de4c4221267a3fe77af46928b7eca497cd778cd5
tpphuhungbp <tpphuhungbp2@gmail.com>
2023-04-12 20:47:15 +0700

    voice

---
+++ Front_end/src/pages/Voice/index.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+
+import styles from "./voice.module.scss";
+
+import styless from "";
+function Voice(){
+  return <h1 className=''>Hello World</h1>
+}
+
+export default Voice;
