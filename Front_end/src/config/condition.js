0b123d1a24a8cdaeb2a236ed820855bfea1d2ded
lehung <lehung16082002@gmail.com>
2023-04-26 23:27:33 +0700

    BUILD: add socket for real updating
    - add socket for dashboardscreen + modescreen
    - add socket for web mode setting

--- Front_end/src/config/condition.js
+++ Front_end/src/config/condition.js
@@ -1,15 +1,18 @@
 const conditions = {
   temperature: {
     unit:"°C",
-    title: 'Temperature'
+    title: 'Temperature',
+    color: "#D0EFF5",
   },
   light:  {
     unit:"W/m²",
-    title: 'Lighting'
+    title: 'Lighting',
+    color: "#D1EEDB",
   },
   moisture:{
     unit:"%",
-    title: 'Soil moisture'
+    title: 'Soil moisture',
+    color: "#F0F6D2",
   },
 };
 
