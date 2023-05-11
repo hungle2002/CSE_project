8fa2953b02114eb4bc4c805154d9026cf341fda6
unknown <khangpro31072002@gmail.com>
2023-05-11 13:12:35 +0700

    change ada key

--- Back_end/build/utils/httpRequestAda.js
+++ Back_end/build/utils/httpRequestAda.js
@@ -9,7 +9,7 @@ const axios_1 = __importDefault(require("axios"));
 const request = axios_1.default.create({
     baseURL: 'https://io.adafruit.com/api/v2/heriota/',
     headers: {
-        'X-AIO-KEY': 'aio_giQK196WcAFqqarqFu7h0O9h23gy',
+        'X-AIO-KEY': 'aio_Tqpj85D27Vub091fLjZX0xfpGyZt',
     },
 });
 async function getFeed(path, options = undefined) {
